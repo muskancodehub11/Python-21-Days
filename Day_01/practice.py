2d7920d7099b3b586a6e1cdfb58e2b82556fad62
Muskan Pal <palmuskan809@gmail.com>
2026-07-26 13:01:27 +0530

    Day 1: Python Basics and Age Calculator

---
+++ Day_01/practice.py
@@ -0,0 +1,21 @@
+#Print your name
+print("Muska Pal")
+name = "Muskan Pal"
+print(f'Hello, {name}!')
+
+#Add two numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+sum = num1 + num2
+print(f"The sum of {num1} and {num2} is {sum}")
+
+
+#Swap two numbers.
+num1,num2 = num2,num1
+print(f'After swaping numbers, first number ={num1}\n second number ={num2}')
+
+#Calculate the area of a rectangle.
+lenght = int(input("ENter the lenght:"))
+width = int(input("Enter th width:"))
+area = lenght * width
+print(f' the area of rectangle is {area}')
